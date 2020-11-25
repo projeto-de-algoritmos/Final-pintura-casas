75339aa13ef46a15687bd0e6e3b857b835a70490
phe0 <pheofo@gmail.com>
2020-11-25 17:15:55 -0300

    mock generate Graph function

---
+++ src/algorithm/generateGraph.js
@@ -0,0 +1,17 @@
+export default function generateGraph() {
+    const graph = new MyGraph();
+
+    graph.addNode(1)
+    graph.addNode(2)
+    graph.addNode(3)
+    graph.addNode(4)
+    graph.addNode(5)
+
+    graph.addEdge(1, 2)
+    graph.addEdge(2, 3)
+    graph.addEdge(2, 4)
+    graph.addEdge(2, 5)
+    graph.addEdge(3, 4)
+
+    return graph;
+}
