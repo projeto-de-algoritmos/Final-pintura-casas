d77170b38f1e5d58665820c0268d79e6512bdcc0
EsioFreitas <esio.gustavo@gmail.com>
2020-11-26 13:40:27 -0300

    Get all possible edges

--- src/algorithm/generateGraph.js
+++ src/algorithm/generateGraph.js
@@ -3,13 +3,23 @@ import Graph from "./Graph";
 export default function generateGraph() {
   const graph = new Graph();
 
-  const graphSize = Math.floor(Math.random() * 10) + 1;
+  const graphSize = Math.floor(Math.random() * 10) + 2;
 
   for (let index = 1; index <= graphSize; index++) {
     graph.addNode(index);
   }
 
- 
+  const edgeAmount = getEdgeAmount(graphSize);
+
+  //   graph.addEdge(1, 2);
+  //   graph.addEdge(2, 3);
+  //   graph.addEdge(2, 4);
+  //   graph.addEdge(2, 5);
+  //   graph.addEdge(3, 4);
 
   return graph;
+}
+
+function getEdgeAmount(graphSize) {
+  return (graphSize * (graphSize - 1)) / 2;
 }
