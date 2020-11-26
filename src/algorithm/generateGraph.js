bb7d526ba3cfeb8735aaed4b3c04868bde773478
EsioFreitas <esio.gustavo@gmail.com>
2020-11-26 14:03:52 -0300

    Add dynamic edges

--- src/algorithm/generateGraph.js
+++ src/algorithm/generateGraph.js
@@ -3,31 +3,16 @@ import Graph from "./Graph";
 export default function generateGraph() {
   const graph = new Graph();
 
-  const graphSize = Math.floor(Math.random() * 10) + 2;
+  const graphSize = Math.floor(Math.random() * 20) + 2;
 
   for (let index = 1; index <= graphSize; index++) {
     graph.addNode(index);
   }
 
-  const edgeAmount = getEdgeAmount(graphSize);
-
-  let node = 1;
-  let neighbor = 2;
-
-  for (let index = 1; index <= edgeAmount; index++) {
-    if (neighbor === node) continue;
-
+  for (let node = 1; node <= graphSize; node++) {
+    const neighbor = Math.floor(Math.random() * (graphSize - 1)) + 1;
     graph.addEdge(node, neighbor);
-
-    if (neighbor === graphSize) {
-      node++;
-      neighbor = node + 1;
-    } else neighbor++;
   }
 
   return graph;
-}
-
-function getEdgeAmount(graphSize) {
-  return (graphSize * (graphSize - 1)) / 2;
 }
